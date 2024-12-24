5f57e3b188e13d73fde89f1c4d5fb6aae5f71e2c
VadimDun <V_ad_im3704@mail.ru>
2024-12-24 09:51:27 +0300

    исправил отображение победителя

--- ui/win/win-screen.gd
+++ ui/win/win-screen.gd
@@ -2,9 +2,10 @@ extends Control
 
 
 func set_winner(player_id: int) -> void:
-	player_id = (player_id + 1) % 2
+	player_id = player_id + 1
 	$PanelContainer/VBoxContainer/WinnetText.text = "Игрок " + str(player_id) + " победил"
+
 
 # переход в лобби
 func _on_button_pressed() -> void:
-	pass # Replace with function body.
+	pass  # Replace with function body.
